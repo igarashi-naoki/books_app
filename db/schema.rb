3022f4e9748123f679f4336ac4fd4d4b9d5e36b3
igarashi-naoki <naoki17igarashi@gmail.com>
2023-09-17 03:33:17 +0900

    1週目動かした分

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_071008) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_16_181757) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "picture"
   end
 
 end
